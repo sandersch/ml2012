d8e372aae2bbed07e5ff20d81c8e9fc2b83a2663
Charlie Sanders <sanderscharlie@gmail.com>
2012-08-23 00:43:05 -0500

    Adds solution for Part 2/7: Computing Cost (single variable)
    Score: 40.00 / 40

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,9 +13,12 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+h = X * theta;
+total = 0;
+for i = 1:m
+    total += (h(i) - y(i)) ** 2;
+end
+J = 1/(2 * m) * total
 
 % =========================================================================
 
