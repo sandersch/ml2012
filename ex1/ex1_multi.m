5305acb95f1477d4ace416ff80dd507bd5aac0f0
Charlie Sanders <sanderscharlie@gmail.com>
2012-08-23 23:36:55 -0500

    Removes pauses from runner ex1_multi

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -43,9 +43,6 @@ m = length(y);
 fprintf('First 10 examples from the dataset: \n');
 fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
 
@@ -111,9 +108,6 @@ price = 0; % You should change this
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
 
 %% ================ Part 3: Normal Equations ================
 
